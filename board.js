3f6f817d0cfc71eb38d94ac766d04943501af5c1
Jake Scott <scottj@sou.edu>
2020-08-20 15:44:48 -0700

    save

---
+++ board.js
@@ -0,0 +1,12 @@
+class Board {
+    ctxBoard;
+    ctxNext;
+    ctxHeld;
+    board;
+    constructor(ctxBoard, ctxNext, ctxHeld) {
+        this.ctxBoard = ctxBoard;
+        this.ctxNext = ctxNext;
+        this.ctxHeld = ctxHeld;
+
+    }
+}
